39028401fbf84de66d0dc7c19ef5ef467beaf31d
변경연 <kissob4905@gmail.com>
2022-03-15 20:12:02 +0900

    fix: post entity null value ignore

--- api/src/main/java/com/simple/board/post/entity/Post.java
+++ api/src/main/java/com/simple/board/post/entity/Post.java
@@ -1,14 +1,17 @@
 package com.simple.board.post.entity;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 
 @Getter
 @Setter
+@DynamicInsert
+@DynamicUpdate
 @Entity
 public class Post {
 
